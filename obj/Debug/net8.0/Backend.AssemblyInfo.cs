985248f66942c38606a4f27af5696a02b687524d
Kristinashahi <128054496+Kristinashahi@users.noreply.github.com>
2025-05-06 22:36:28 +0545

    password hashing

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86869f548b3ca4ef69b20ffd3627209eb4eef57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+565236bc38567878173a6b4ec125a765aaa0f85a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
